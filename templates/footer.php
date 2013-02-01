ca7e44c260aa324660d4c5bc8a99cac55d6261b5
Mark Bates <mbates@unimelb.edu.au>
2013-02-01 16:56:02 +1100

    display version in footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,12 @@
 <?php
+$version = $c->get('version');
 echo <<<EOF
 	</div>
 </div>
 <hr>
 <footer>
 	<div class="container">
-		<p class="muted credit">Enrol Software by <a href="http://github.com/nokobe">Mark Bates</a></p>
+		<p class="muted credit">Enrol Software $version by <a href="http://github.com/nokobe">Mark Bates</a></p>
 	</div>
 </footer>
 <script src="$t->base/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
